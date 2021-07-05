2e337c830b84c3a33d179b7c97e32d9020f7728f
KonstantinGasser <konstantinfranzkorbinian.gasser@stud.hs-mannheim.de>
2021-07-05 04:02:41 +0200

    v0.2.0

---
+++ sherlock.rb
@@ -0,0 +1,10 @@
+class sherlock < Formula
+    desc "easy and simple to use cli password manager"
+    homepage: "https://github.com/KonstantinGasser/sherlock"
+    url: "https://github.com/KonstantinGasser/sherlock/archive/refs/tags/v0.2.0.tar.gz"
+    sha254: "9476a2071975b4afb5b65bb37688050a6bb524d78925cd9120e25e4d978cf51a"
+    version: "0.2.0"
+    def install
+        bin.install "sherlock"
+    end
+end
