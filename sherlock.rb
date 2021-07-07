233f36c624cacd0c64969bc4eeb3b77e004be7e7
KonstantinGasser <konstantinfranzkorbinian.gasser@stud.hs-mannheim.de>
2021-07-07 16:42:44 +0200

    updated fetch url for sherlock

--- sherlock.rb
+++ sherlock.rb
@@ -1,7 +1,7 @@
 class Sherlock < Formula
     desc "easy and simple to use cli password manager"
     homepage "https://github.com/KonstantinGasser/sherlock"
-    url "https://github.com/KonstantinGasser/sherlock/archive/refs/tags/v0.2.0.tar.gz"
+    url "https://github.com/KonstantinGasser/sherlock/releases/download/v0.2.0/sherlock-darwin.tar.gz"
     sha256 "b24cd2c406c0a75e309f073ec50fb3cf1a17b1bdc2b97e195111ec7e68932757"
     version "0.2.0"
     def install
