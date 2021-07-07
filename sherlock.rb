541ac2aeab686569f06bc2311f6272c694739b21
KonstantinGasser <43576797+KonstantinGasser@users.noreply.github.com>
2021-07-07 16:45:10 +0200

    Update sherlock.rb
    fixed invalid sha256

--- sherlock.rb
+++ sherlock.rb
@@ -2,7 +2,7 @@ class Sherlock < Formula
     desc "easy and simple to use cli password manager"
     homepage "https://github.com/KonstantinGasser/sherlock"
     url "https://github.com/KonstantinGasser/sherlock/releases/download/v0.2.0/sherlock-darwin.tar.gz"
-    sha256 "b24cd2c406c0a75e309f073ec50fb3cf1a17b1bdc2b97e195111ec7e68932757"
+    sha256 "46726a56c32bae943cab6fb906d724a6d4f54aefc8b229baf3c9624678a22d41"
     version "0.2.0"
     def install
         bin.install "sherlock"
