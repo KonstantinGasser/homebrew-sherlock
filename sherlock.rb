c4d0720a9581607556cbb4edd49c4edcf06c7c50
KonstantinGasser <konstantinfranzkorbinian.gasser@stud.hs-mannheim.de>
2021-07-13 22:38:28 +0200

    how did this happend sha57...

--- sherlock.rb
+++ sherlock.rb
@@ -2,7 +2,7 @@ class Sherlock < Formula
     desc "easy and simple to use cli password manager"
     homepage "https://github.com/KonstantinGasser/sherlock"
     url "https://github.com/KonstantinGasser/sherlock/releases/download/v0.3.1/sherlock-darwin.tar.gz"
-    sha257 "b5144c5b6a62bc0e8579892cc725e8ad144b92cbbfebf3a04d57491cbd0c6b6f"
+    sha256 "b5144c5b6a62bc0e8579892cc725e8ad144b92cbbfebf3a04d57491cbd0c6b6f"
     version "0.3.1"
     def install
         bin.install "sherlock"
