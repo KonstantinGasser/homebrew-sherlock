baf25afa3d9912c2187479330bb43702c5a2d04d
KonstantinGasser <konstantinfranzkorbinian.gasser@stud.hs-mannheim.de>
2021-09-23 21:11:29 +0200

    v0.3.3

--- sherlock.rb
+++ sherlock.rb
@@ -1,9 +1,9 @@
 class Sherlock < Formula
     desc "easy and simple to use cli password manager"
     homepage "https://github.com/KonstantinGasser/sherlock"
-    url "https://github.com/KonstantinGasser/sherlock/releases/download/v0.3.2/sherlock-darwin.tar.gz"
-    sha256 "a9912cb85f29dcb09639db19c5f9b3dd590d310324e2d3083d69ad36a2ced9fc"
-    version "0.3.1"
+    url "https://github.com/KonstantinGasser/sherlock/releases/download/v0.3.3/sherlock-darwin.tar.gz"
+    sha256 "17357b7eaf79ec30ced9548c4a3cb53c6d8ceadd67b0c1ccb74a8fd0245b2210"
+    version "0.3.3"
     def install
         bin.install "sherlock"
     end
