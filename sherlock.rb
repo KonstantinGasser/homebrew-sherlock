4139db6b5a09a7bcbf711500ce182b68f0e40197
KonstantinGasser <konstantin.gasser@me.com>
2022-02-04 20:21:40 +0100

    release v0.4.0 version missmatch in ruby class

--- sherlock.rb
+++ sherlock.rb
@@ -1,7 +1,7 @@
 class Sherlock < Formula
     desc "easy and simple to use cli password manager"
     homepage "https://github.com/KonstantinGasser/sherlock"
-    url "https://github.com/KonstantinGasser/sherlock/releases/download/v0.3.3/sherlock-darwin.tar.gz"
+    url "https://github.com/KonstantinGasser/sherlock/releases/download/v0.4.0/sherlock-darwin.tar.gz"
     sha256 "d65ab3b466a300ef77461faf9f2e4bb4febeaee46c6c28debbb1db32b22892bb"
     version "0.4.0"
     def install
