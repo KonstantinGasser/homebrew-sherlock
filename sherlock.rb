919a394bf05b26bc7b4a378ab002a5e87fb023a0
KonstantinGasser <konstantin.gasser@me.com>
2022-02-18 11:07:24 +0100

    patch v0.4.1

--- sherlock.rb
+++ sherlock.rb
@@ -1,9 +1,9 @@
 class Sherlock < Formula
     desc "easy and simple to use cli password manager"
     homepage "https://github.com/KonstantinGasser/sherlock"
-    url "https://github.com/KonstantinGasser/sherlock/releases/download/v0.4.0/sherlock-darwin.tar.gz"
-    sha256 "d65ab3b466a300ef77461faf9f2e4bb4febeaee46c6c28debbb1db32b22892bb"
-    version "0.4.0"
+    url "https://github.com/KonstantinGasser/sherlock/releases/download/v0.4.1/sherlock-darwin.tar.gz"
+    sha256 "31cf791a8d1d070bc6323975292fed96bc0c063a2f8d6da1c2566531b7c6f482"
+    version "0.4.1"
     def install
         bin.install "sherlock"
     end
